d65623b4987db506128a1402ac226d5bf30cd814
Salucker <54082098+Salucker@users.noreply.github.com>
2021-05-24 21:45:42 +0400

    Delete output.php

--- MySite/php/output.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once 'connect.php';
-
-	$query = mysqli_query ($connect, "SELECT * FROM `homeinternet`");
-	$tariffs = mysqli_fetch_all($query, MYSQLI_ASSOC);//результат sql в массив
-?>
